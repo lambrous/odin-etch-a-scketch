364e23c73bc1c8fd0cd7443688956596794ab238
Lambrous <lambrous@outlook.ph>
2024-03-18 14:36:18 +0800

    Add hover effect and drawing trail functionality to grid divs

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ const container = document.querySelector(".container");
 for (let i = 0; i < gridSize; i++) {
 	const block = document.createElement("div");
 	block.classList.add("block");
+	block.addEventListener("mouseover", etch);
 	container.append(block);
+}
+
+function etch() {
+	this.classList.add("etch");
 }
