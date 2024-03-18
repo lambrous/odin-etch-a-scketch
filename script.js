bdedd7aadce1a7d4d03388a01aeed18dd9e34bb2
Lambrous <lambrous@outlook.ph>
2024-03-18 15:30:39 +0800

    Implement grid resize functionality with user input for new dimensions

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-const gridSize = 16 ** 2;
 const container = document.querySelector(".container");
+const resizeBtn = document.querySelector(".resize-btn");
 
-for (let i = 0; i < gridSize; i++) {
-	const block = document.createElement("div");
-	block.classList.add("block");
-	block.addEventListener("mouseover", etch);
-	container.append(block);
+resizeBtn.addEventListener("click", resizeGrid);
+
+function generateGrid(colSize = 16) {
+	const gridSize = colSize ** 2;
+	container.style.width = `${colSize * 10}px`;
+
+	for (let i = 0; i < gridSize; i++) {
+		const block = document.createElement("div");
+		block.classList.add("block");
+		block.style.flexBasis = `calc(100% / ${colSize})`;
+		block.addEventListener("mouseover", etch);
+		container.append(block);
+	}
+}
+
+function removeGrid() {
+	const blocks = document.querySelectorAll(".block");
+	for (const block of blocks) {
+		block.remove();
+	}
 }
 
 function etch() {
 	this.classList.add("etch");
 }
+
+function resizeGrid() {
+	const maxSize = 100;
+	let colSize = 16;
+
+	do {
+		colSize = +prompt(
+			"How many squares per side would you like for the new grid?",
+			colSize,
+		);
+	} while (colSize > maxSize || Number.isNaN(colSize));
+
+	removeGrid();
+	generateGrid(colSize);
+}
+
+generateGrid();
