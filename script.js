afea412c34d45063c9b0f2ef96f1e083639ec217
Lambrous <lambrous@outlook.ph>
2024-03-18 14:23:42 +0800

    Implemented 16x16 generated divs using flexbox for layout

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("HELLO");
+const gridSize = 16 ** 2;
+const container = document.querySelector(".container");
+
+for (let i = 0; i < gridSize; i++) {
+	const block = document.createElement("div");
+	block.classList.add("block");
+	container.append(block);
+}
